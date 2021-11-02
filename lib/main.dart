00f7278d4a608c0ab02c845a808a6d6580bcbddd
Your Name <you@example.com>
2021-11-02 20:44:34 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
                   textScaleFactor: 1.25,
                 ),
                 child: PhoneLogin());
-          }
+          } //lkhlkhlkh
         }));
   }
 }
